3b11390f3a2d44c5acdf222167ac72804eb990f2
grantbrewster <gdbrewster@gmail.com>
2020-08-29 23:48:39 -0400

    Updates

---
+++ precache-manifest.249215a173ac4530f3086e4bf9c00c8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af107fc5b3c7fe3d2a0c9978c6377d7",
+    "url": "/grant/index.html"
+  },
+  {
+    "revision": "2c8bd1273032c051a41c",
+    "url": "/grant/static/css/main.2114bd5e.chunk.css"
+  },
+  {
+    "revision": "86291f3f467a5e410877",
+    "url": "/grant/static/js/2.c2ecc02d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/grant/static/js/2.c2ecc02d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c8bd1273032c051a41c",
+    "url": "/grant/static/js/main.f7640613.chunk.js"
+  },
+  {
+    "revision": "07d1275e84880d0474c7",
+    "url": "/grant/static/js/runtime-main.ba34f99e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0a60bde5e97891dc2f67b67a98840695.js"
+  "/grant/precache-manifest.249215a173ac4530f3086e4bf9c00c8b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/grant/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
